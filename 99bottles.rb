4967127e65cb3e1fe9c7359e5f6b525d5fe46ba5
Damian Sieroń <damiansieron94@gmail.com>
2018-07-10 11:20:56 +0200

    99bottles

--- 99bottles.rb
+++ 99bottles.rb
@@ -1,25 +1,28 @@
-iteration_number = 200
+iteration_number = 99
 
-numberToString = ['zero', 'one', 'two', 'three', 'four', 'five']
-
-if iteration_number > 5
-  for i in (iteration_number).downto(5)
-      puts("#{i} bottles of beer on the wall, #{i} bottles of beer.")
-      puts("Take one down and pass it around, #{i - 1} bottles of beer on the wall.")
-  end
-  for i in(5).downto(0)
-    if i == 2
-      puts("#{numberToString[i].capitalize} bottles of beer on the wall, #{numberToString[i]} bottles of beer.")
-      puts("Take one down and pass it around, #{numberToString[i - 1]} bottle of beer on the wall.")
-    elsif i == 1
-      puts("#{numberToString[i].capitalize} bottle of beer on the wall, #{numberToString[i]} bottle of beer.")
-      puts("Take one down and pass it around, no more bottles of beer on the wall.")
-    elsif i == 0
-      puts("No more bottles of beer on the wall, no more bottles of beer.")
-      puts("Go to the store and buy some more, #{iteration_number} bottles of beer on the wall.")
-    else
-      puts("#{numberToString[i].capitalize} bottles of beer on the wall, #{numberToString[i]} bottles of beer.")
-      puts("Take one down and pass it around, #{numberToString[i - 1]} bottles of beer on the wall.")
-    end
+def get_number(number)
+  number_to_string = {1 => 'one', 2 => 'two', 3 => 'three', 4 => 'four', 5 => 'five'}
+  if(number <= 5)
+    return number_to_string[number]
+  else
+    return number
   end
 end
+
+def get_form(number)
+  if number == 1
+    return 'bottle'
+  else
+    return 'bottles'
+  end
+end
+
+for i in (iteration_number).downto(2)
+  puts("#{get_number(i)} #{get_form(i)} of beer on the wall, #{get_number(i)} #{ get_form(i)} of beer.")
+  puts("Take one down and pass it around, #{get_number(i - 1)} #{get_form(i - 1)}  of beer on the wall.")
+end
+
+puts ("#{get_number(1)} #{get_form(1)} bottle of beer on the wall, #{get_number(1)} #{get_form(1)} of beer.")
+puts ("Take one down and pass it around, no more bottles of beer on the wall.")
+puts("No more bottles of beer on the wall, no more bottles of beer.")
+puts("Go to the store and buy some more, #{iteration_number} #{get_form(iteration_number)} of beer on the wall.")
