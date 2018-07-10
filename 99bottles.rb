526a7948c218d5b6f4ba9a72ebbe3ee5870ac886
Damian Sieroń <damiansieron94@gmail.com>
2018-07-10 07:06:50 +0200

    file reader

--- 99bottles.rb
+++ 99bottles.rb
@@ -1,6 +1,6 @@
 iteration_number = 200
 
-numbers = ["zero", "one", "two", "three", "four", "five"]
+numberToString = ['zero', 'one', 'two', 'three', 'four', 'five']
 
 if iteration_number > 5
   for i in (iteration_number).downto(5)
@@ -9,17 +9,17 @@ if iteration_number > 5
   end
   for i in(5).downto(0)
     if i == 2
-      puts("#{numbers[i].to_s.capitalize} bottles of beer on the wall, #{numbers[i]} bottles of beer.")
-      puts("Take one down and pass it around, #{numbers[i - 1]} bottle of beer on the wall.")
+      puts("#{numberToString[i].capitalize} bottles of beer on the wall, #{numberToString[i]} bottles of beer.")
+      puts("Take one down and pass it around, #{numberToString[i - 1]} bottle of beer on the wall.")
     elsif i == 1
-      puts("#{numbers[i].to_s.capitalize} bottle of beer on the wall, #{numbers[i]} bottle of beer.")
+      puts("#{numberToString[i].capitalize} bottle of beer on the wall, #{numberToString[i]} bottle of beer.")
       puts("Take one down and pass it around, no more bottles of beer on the wall.")
     elsif i == 0
       puts("No more bottles of beer on the wall, no more bottles of beer.")
       puts("Go to the store and buy some more, #{iteration_number} bottles of beer on the wall.")
     else
-      puts("#{numbers[i].to_s.capitalize} bottles of beer on the wall, #{numbers[i]} bottles of beer.")
-      puts("Take one down and pass it around, #{numbers[i - 1]} bottles of beer on the wall.")
+      puts("#{numberToString[i].capitalize} bottles of beer on the wall, #{numberToString[i]} bottles of beer.")
+      puts("Take one down and pass it around, #{numberToString[i - 1]} bottles of beer on the wall.")
     end
   end
 end
--- levenshtein_distance.rb
+++ levenshtein_distance.rb
@@ -1,22 +1,41 @@
-string_1 = ARGV[0]
-string_2 = ARGV[1]
-
-array = Array.new(n + 1){Array.new(m + 1)}
-
-for i in 0..m
-    array[i][0] = i
-end
-
-for j in 1..n
-    array[0][j] = j
-end
-
-for i in 1..m
-  for j in 1..n
-    cost = (string_1[i] == string_2[j]) ? 0 : 1
-    array[i][j] = [array[i - 1][j] + 1, array[i][j - 1] + 1,
-                  array[i - 1][j - 1] + cost].min
+def read_file(file_name)
+  data = Array.new
+  file = File.open(file_name).read
+  file.each_line do |line|
+    data.push(line)
   end
+  return data
 end
 
-puts array[m][n]
+def calculate_levenshtein_distance(string_1, string_2)
+  n = string_1.length
+  m = string_2.length
+  array = Array.new(m + 1){Array.new(n + 1)}
+
+  for i in 0..m
+    array[i][0] = i
+  end
+
+  for j in 1..n
+    array[0][j] = j
+  end
+
+  for i in 1..m
+    for j in 1..n
+      cost = (string_1[i] == string_2[j]) ? 0 : 1
+      array[i][j] = [array[i - 1][j] + 1, array[i][j - 1] + 1,
+      array[i - 1][j - 1] + cost].min
+    end
+  end
+
+  return  array[m][n]
+end
+
+puts "String_1: #{ARGV[0]}"
+puts "String_2: #{ARGV[1]}"
+puts "Cost: #{ calculate_levenshtein_distance(ARGV[0], ARGV[1]) }"
+
+data = read_file('words_levenshtein_distance')
+puts "String_1: #{data[0]}"
+puts "String_2: #{data[1]}"
+puts "Cost: #{ calculate_levenshtein_distance(data[0], data[1]) }"
